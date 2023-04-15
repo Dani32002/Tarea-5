9be96495b05c7a7cf9372d4bf56e69fe66c52e82
sgoncalves13 <s.goncalves@uniandes.edu.co>
2023-04-15 14:07:23 -0500

    configuraciones ejecucion

--- Tarea 5/src/Parte1/Ejercicio1.java
+++ Tarea 5/src/Parte1/Ejercicio1.java
@@ -6,10 +6,12 @@ import java.util.ArrayList;
 import modulo.Carga;
 
 public class Ejercicio1 {
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        Carga carga = new Carga();
-		Prim calculadora = new Prim();
-        ArrayList<ArrayList<Integer>> matriz = carga.cargarMatriz(args[0]);
+    public static void main(String[] args) throws NumberFormatException, IOException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+        Carga carga = new Carga();		
+		String ejeArbol = Ejercicio1.class.getPackage().getName() + "." + args[0];
+		@SuppressWarnings("deprecation")
+		ArbolRecubrimiento calculadora = (ArbolRecubrimiento) Class.forName(ejeArbol).newInstance();
+        ArrayList<ArrayList<Integer>> matriz = carga.cargarMatriz(args[1]);
 
         long inicioTiempo = System.currentTimeMillis();
         ArrayList<ArrayList<Integer>> arb_rec = calculadora.ejesArbol(matriz);
